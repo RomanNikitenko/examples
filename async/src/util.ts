960c2a3645850d683b24f4a9fea076b5d7434cc1
Roman Nikitenko <rnikiten@redhat.com>
2021-10-23 09:03:20 +0300

    Improve doc for functions
    Signed-off-by: Roman Nikitenko <rnikiten@redhat.com>

--- async/src/util.ts
+++ async/src/util.ts
@@ -1,6 +1,6 @@
 /**
- * Creates a new function which wraps a target function and contains timeout. The timeout is started when the target function is called.
- * The resulting promise is rejected by timeout when execution of the target function takes more time than given timeout.
+ * Creates a new function which wraps a given target function and contains timeout. The timeout is started when the target function is called.
+ * The resulting promise is rejected by timeout when execution of the target function takes more time than the given timeout.
  * @returns A new function with timeout.
  */
 export function executeOrTimeout<T>(originalFunc: (...args) => Promise<T>, ms: number, timeoutErrorMessage: string): Function {
@@ -15,7 +15,11 @@ export function executeOrTimeout<T>(originalFunc: (...args) => Promise<T>, ms: n
     return fn;
 }
 
-/** Creates a Promise that is resolved or rejected by timeout. */
+/** 
+ * The function gets a target Promise and creates a new Promise with timeout. 
+ * The resulting promise is resolved or rejected when any of the Promises are resolved or rejected. 
+ * So, it's rejected by timeout when execution of the target Promise takes more time than the given timeout. 
+ */
 export function promiseWithTimeout<T>(targetPromise: Promise<T>, ms: number, timeoutErrorMessage: string): Promise<T> {
     const timeoutPromise = new Promise<never>((unused, reject) => {
         setTimeout(() => { reject(new Error(timeoutErrorMessage)) }, ms);
