811e40759db04dd2a076b3e33803ce4bdcd5e87c
Roman Nikitenko <rnikiten@redhat.com>
2024-02-23 20:17:16 +0200

    Fix formatting
    Signed-off-by: Roman Nikitenko <rnikiten@redhat.com>

--- ts/console/src/readfile.ts
+++ ts/console/src/readfile.ts
@@ -9,29 +9,29 @@ import * as fs from 'fs';
 
 
 function extractTokensFromFile(filePath: string): string[] {
-	try {
-		const fileContent = fs.readFileSync(filePath, 'utf8');
-		const lines = fileContent.split('\n');
-		const tokens: string[] = [];
+  try {
+    const fileContent = fs.readFileSync(filePath, 'utf8');
+    const lines = fileContent.split('\n');
+    const tokens: string[] = [];
 
-		console.log(`=== Found ${lines.length} lines in the file`);
-		for (const line of lines) {
-			console.log(`============== Line: ${line}`);
-			if (!line.includes('gitlab.com')) {
-				console.log(`=== The line contains NOT gitlab.com token`);
-				const token = line.substring(line.lastIndexOf(':') + 1, line.indexOf('@'));
-				console.log(`=== Token: ${token}`);
-				tokens.push(token); 
-			} else {
-				console.log(`=== The line contains gitlab.com token, so skip it`);
-			}
-		}
+    console.log(`=== Found ${lines.length} lines in the file`);
+    for (const line of lines) {
+      console.log(`============== Line: ${line}`);
+      if (!line.includes('gitlab.com')) {
+        console.log(`=== The line contains NOT gitlab.com token`);
+        const token = line.substring(line.lastIndexOf(':') + 1, line.indexOf('@'));
+        console.log(`=== Token: ${token}`);
+        tokens.push(token);
+      } else {
+        console.log(`=== The line contains gitlab.com token, so skip it`);
+      }
+    }
 
-		return tokens;
-	} catch (err) {
-		console.error('Error reading the file:', err);
-		return [];
-	}
+    return tokens;
+  } catch (err) {
+    console.error('Error reading the file:', err);
+    return [];
+  }
 }
 
 const filePath = './src/tokens.txt';
